68fccea6cae185c99511d304b657d3746ce5d51d
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-04-10 23:39:42 -0700

    add new python resources

---
+++ Resouces.py
@@ -0,0 +1,5 @@
+http://www.techbeamers.com/selenium-python-test-suite-unittest/
+https://learnxinyminutes.com/docs/python3/
+https://www.seleniumhq.org/docs/04_webdriver_advanced.jsp
+https://www.seleniumhq.org/docs/03_webdriver.jsp
+https://github.com/christian-bromann/awesome-selenium
