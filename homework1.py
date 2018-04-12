b30dd569e9dfe28059782fa1105973d6c65638c9
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-04-11 23:02:45 -0700

    homework1

---
+++ homework1.py
@@ -0,0 +1,32 @@
+#https://learnxinyminutes.com/docs/python3/
+#read the article about
+#create a function call mean
+
+def sum(inputList):
+    x = 0
+    for y in inputList:
+        x = x + y
+    return x
+
+print(sum([1, 2, 4]))
+
+#mean is sum/num, complete mean function and print out result of  mean([2, 4, 5, 6])
+def mean(inputList)
+
+
+
+#define a squart function.    squart([2, 4, 5, 6]) must return [4, 16, 25, 36]
+
+
+
+#define sumodd function to sum all odd number in a list, oddsum([1, 3, 4, 5, 6]) must result 9
+
+
+#define listDir function to list content of a directory, for example, listDir("c:/") must print out all files in the path c:/, google to find how to do it
+
+
+#define readExcel function to read and print content of excel,  readExcel("c:/myexcel.xls", 0) will read sheet 0 content, using xlrd library
+
+
+
+
