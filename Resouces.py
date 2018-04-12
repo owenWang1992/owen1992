838b84470875ddd746d53057a9fbfc9babe1c419
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-04-11 22:08:19 -0700

    change

--- Resouces.py
+++ Resouces.py
@@ -3,3 +3,11 @@ https://learnxinyminutes.com/docs/python3/
 https://www.seleniumhq.org/docs/04_webdriver_advanced.jsp
 https://www.seleniumhq.org/docs/03_webdriver.jsp
 https://github.com/christian-bromann/awesome-selenium
+
+git commandline flow:
+    git status  #check current status, allways run it before any other commands
+    git add .  #only use if you just add a new file
+    git commit -am"your comments"
+    git pull   # pull new update from remote
+    git push   #push to the remote
+
