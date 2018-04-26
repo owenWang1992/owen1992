4a420e1d9915ee3a199841b2a6475bb4ae4a274b
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-04-26 16:22:52 -0700

    change style

--- optool.py
+++ optool.py
@@ -10,12 +10,12 @@ import time
 '''
 Navigation to target page
 Grab all op links in page and print out.
-Assert offer and op match each other based on a opTable dictionary.
+Assert offer and op match each other based on a op_table dictionary.
 Assert no unrecognized offer and op pairs
 Assert Campaign field is not "XXXX"
 Test passed if all assert pass, otherwise failed.
 
-if new offer and op pairs are found, print out so that they can be copied and added to opTable
+if new offer and op pairs are found, print out so that they can be copied and added to op_table
 
 '''
 
@@ -30,66 +30,21 @@ class LandingPageTest(unittest.TestCase):
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(10)
         self.mobile = False
-        self.opTable = {
-            'at_fcras100': 'FRCR-PRD-PCO-100', 
-            'at_eiwpd102': 'W3DC-PRD-PCO-102', 
-            'at_eiwa104': 'WPAC-PRD-PCO-104', 
-            'at_eiwb105': 'WPMC-PRD-PCO-105', 
-            'at_eiwpa106': 'W3AC-PRD-PCO-106', 
-            'at_1b1s109': '1B1S-PRD-PCO-109', 
+        self.op_table = {
+            'at_fcras100': 'FRCR-PRD-PCO-100',
+            'at_eiwpd102': 'W3DC-PRD-PCO-102',
+            'at_eiwa104': 'WPAC-PRD-PCO-104',
+            'at_eiwb105': 'WPMC-PRD-PCO-105',
+            'at_eiwpa106': 'W3AC-PRD-PCO-106',
+            'at_1b1s109': '1B1S-PRD-PCO-109',
             'at_3b3s110': '3B3S-PRD-PCO-110'
-            }
-        
-    def getPlacementsFromPage(self):
-        time.sleep(5)
-        links = self.driver.find_elements(By.CSS_SELECTOR, "#main a.btn")
-        placements = []
-        for link in links:
-            url = link.get_attribute("href")
-            a = url.split('?')
-            placement = dict(parse.parse_qsl(a[1]))
-            placement["url"] = url
-            placement["title"] = link.get_attribute("innerText")
-            placement["hidden"] = False if link.is_displayed() else True
-            if placement["op"]:
-                opComponents = placement["op"].split("-")
-                placement["op_offer"] = "-".join(opComponents[0:4])
-                placement["op_postion"] = "-".join(opComponents[4:5])
-                placement["op_campaign"] = "-".join(opComponents[5:6])
-                placement["op_AB"] = "-".join(opComponents[6:7])
-                placement["op_source"] = "-".join(opComponents[7:8])
-                placement["op_platform"] = "-".join(opComponents[8:9])      
-            placements.append(placement)
-        return placements
+        }
 
-
-    def checkNewOffer(self, placements):
-        newOffer = {}
-        for placement in placements:
-            if placement["offer"] not in self.opTable:
-                newOffer[placement["offer"]] = placement["op_offer"]
-        if newOffer:
-            print("***find new offers, please add them to opTable in the code.")
-            print(newOffer)
-
-    def checkPage(self):
-        placements = self.getPlacementsFromPage()
-        for placement in placements:
-            print("{:20s} {} {}".format(placement["title"], placement["url"], "\tH" if placement["hidden"] else ""))
-        print("")
-        self.checkNewOffer(placements)  
-        for placement in placements:
-            self.assertEqual(self.opTable[placement["offer"]], placement["op_offer"])
-            self.assertNotEqual(placement["op_campaign"], "XXXX")        
-
-    
-    def doTest(self):        
-        startUrl = "https://www.experian.com"
-        testUrl_A = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products-1.html?pc=dir_exp_0"
-        testUrl_B = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products-cl.html?pc=dir_exp_0"
-        testUrl = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products.html"
-        #self.driver.get(startUrl)
-        self.driver.get(testUrl)
+    def doTest(self):
+        # test_url_A = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products-1.html?pc=dir_exp_0"
+        # test_url_B = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products-cl.html?pc=dir_exp_0"
+        test_url = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products.html"
+        self.driver.get(test_url)
         print(self.driver.current_url)
         self.checkPage()
         planButtons = self.driver.find_elements(
@@ -102,15 +57,67 @@ class LandingPageTest(unittest.TestCase):
     def testDesktop(self):
         print("\nTest Desktop")
         self.doTest()
-      
+
     def testMobile(self):
         print("\nTest Mobile")
         self.driver.set_window_size(375, 812)
         self.doTest()
-    
+
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
+
+    def parseOpLink(self, link):
+        url = link.get_attribute("href")
+        op_data = {}
+        if url and "?" in url:
+            a = url.split('?')
+            op_data = dict(parse.parse_qsl(a[1]))
+            if ("offer" in op_data) and ("op" in op_data):
+                op_data["url"] = url
+                op_data["title"] = link.get_attribute("innerText")
+                op_data["hidden"] = False if link.is_displayed() else True
+                if op_data["op"]:
+                    opComponents = op_data["op"].split("-")
+                    op_data["op_offer"] = "-".join(opComponents[0:4])
+                    op_data["op_postion"] = "-".join(opComponents[4:5])
+                    op_data["op_campaign"] = "-".join(opComponents[5:6])
+                    op_data["op_AB"] = "-".join(opComponents[6:7])
+                    op_data["op_source"] = "-".join(opComponents[7:8])
+                    op_data["op_platform"] = "-".join(opComponents[8:9])
+                    return op_data
+        return {}
+
+    def getOpDataListFromPage(self):
+        time.sleep(5)
+        links = self.driver.find_elements(By.CSS_SELECTOR, "a")
+        op_data_list = []
+        for link in links:
+            op_data = self.parseOpLink(link)
+            if op_data:
+                op_data_list.append(op_data)
+        return op_data_list
+
+    def checkNewOffer(self, op_data_list):
+        new_offer = {}
+        for op_data in op_data_list:
+            if op_data["offer"] not in self.op_table:
+                new_offer[op_data["offer"]] = op_data["op_offer"]
+        if new_offer:
+            print("***find new offers, please add them to op_table in the code.")
+            print(new_offer)
+
+    def checkPage(self):
+        op_data_list = self.getOpDataListFromPage()
+        for op_data in op_data_list:
+            print("{:20s} {} {}".format(
+                op_data["title"], op_data["url"], "\tH" if op_data["hidden"] else ""))
+        print("")
+        self.checkNewOffer(op_data_list)
+        for op_data in op_data_list:
+            self.assertEqual(
+                self.op_table[op_data["offer"]], op_data["op_offer"])
+            self.assertNotEqual(op_data["op_campaign"], "XXXX")
 
 
 if __name__ == '__main__':
