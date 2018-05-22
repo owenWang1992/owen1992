18d9438e0cae3501efd2d344085156cdc6efa7ef
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-05-21 22:04:11 -0700

    add ctalip.py

---
+++ ctalib.py
@@ -0,0 +1,122 @@
+from urllib import parse
+
+offer_table = {
+    'at_frsas102': 'FRSC',
+    'at_frsas103': 'FRSI',
+    'at_fcras100': 'FRCR',
+    'at_fcras105': 'FRRI',
+    'at_fcras102': 'FRCD',
+    'at_ltdreg100': 'FRLR',
+    'at_eiwt100': 'WPTC',
+    'at_eiwt109': 'WPTC',
+    'at_eiwt110': 'WPTC',
+    'at_eiwd100': 'WPDC',
+    'at_eiwb105': 'WPMC',
+    'at_eiwmbg100': 'G1TC',
+    'at_eiwmbg101': 'G1TC',
+    'at_eiwa104': 'WPAC',
+    'at_eiwambg100': 'G1AC',
+    'at_eiwpt104': 'W3TC',
+    'at_eiwpt103': 'W3TC',
+    'at_eiwpt101': 'W3TC',
+    'at_eiwpd102': 'W3DC',
+    'at_eiwp104': 'W3MC',
+    'at_eiwpmbg100': 'G3TC',
+    'at_eiwpa106': 'W3AC',
+    'at_eiwpambg100': 'G3AC',
+    'at_eiwl100': 'WCMI',
+    'at_eiwla100': 'WCAI',
+    'at_eiwt103': 'W1TI',
+    'at_eiwt104': 'W1TI',
+    'at_eiwb103': 'W1MI',
+    'at_eiwb102': 'W1MI',
+    'at_eiwmbg102': 'G1TI',
+    'at_eiwmbg103': 'G1TI',
+    'at_eiwa103': 'W1AI',
+    'at_eiwa102': 'W1AI',
+    'at_eiwambg101': 'G1AI',
+    'at_eiwambg102': 'W1AG',
+    'at_eiwct100': 'W1TK',
+    'at_eiwbc100': 'W1MK',
+    'at_eiwcmbg100': 'G1TK',
+    'at_eiwac100': 'W1AK',
+    'at_eiwacmbg100': 'G1AK',
+    'at_eiwft100': 'W1TF',
+    'at_eiwbf100': 'W1MF',
+    'at_eiwfmbg100': 'G1TF',
+    'at_eiwaf100': 'W1AF',
+    'at_eiwafmbg100': 'G1AF',
+    'at_eiwpt102': 'W3TI',
+    'at_eiwpd101': 'W3DI',
+    'at_eiwpd103': 'W3DI',
+    'at_eiwp103': 'W3MI',
+    'at_eiwp102': 'W3MI',
+    'at_eiwpmbg101': 'W3MG',
+    'at_eiwpa104': 'W3AI',
+    'at_eiwpa103': 'W3AI',
+    'at_eiwpambg101': 'G3AI',
+    'at_eiwpambg102': 'G3AI',
+    'at_eiwpct100': 'W3TK',
+    'at_eiwpbc100': ': W3MK',
+    'at_eiwpcmbg100': ': G3MK',
+    'at_eiwpac100': ': W3AK',
+    'at_eiwpacmbg100': ': G3AK',
+    'at_eiwpft100': ': W3TF',
+    'at_eiwpbf100': ': W3MF',
+    'at_eiwpfmbg100': ': G3MF',
+    'at_eiwpaf100': ': W3AF',
+    'at_eiwpafmbg100': 'G3AF',
+    'at_1b1s108': '1B1S',
+    'at 1b1s109': '1B1S',
+    'at_1b1s109': '1B1S',
+    'at_3b3s109': '3B3S',
+    'at_3b3s102': '3B3S',
+    'at_3b3s113': '3B3S',
+    'at_3b3s100': '3B3S',
+    'at_3b3s110': '3B3S'
+}
+
+
+def parseCTALink(link):
+        url = link.get_attribute("href")
+        cta_data = {}
+        if url and "?" in url:
+            a = url.split('?')
+            cta_data = dict(parse.parse_qsl(a[1]))
+            cta_data["url"] = url
+            cta_data["title"] = link.get_attribute("innerText")
+            cta_data["hidden"] = False if link.is_displayed() else True
+            if "op" in cta_data:
+                opComponents = cta_data["op"].split("-")
+                cta_data["op_product"] = "-".join(opComponents[0:1])
+                cta_data["op_offer_number"] = "-".join(opComponents[3:4])
+                cta_data["op_placement"] = "-".join(opComponents[4:5])
+                cta_data["op_campaign"] = "-".join(opComponents[5:6])
+                cta_data["op_recipe"] = "-".join(opComponents[6:7])
+                cta_data["op_source"] = "-".join(opComponents[7:8])
+                cta_data["op_platform"] = "-".join(opComponents[8:9])
+        return cta_data
+
+def getCTAListFromLinks(links):
+    cta_list = []
+    for link in links:
+        cta_data = parseCTALink(link)
+        if "offer" in cta_data:
+            cta_list.append(cta_data)
+    return cta_list
+
+def checkMissingOp(cta):
+    if "op" in cta:
+        return ""
+    else:
+        return "op is missing in " + cta["url"]
+
+
+def checkOpOffer(cta):
+    if ("offer" in cta) and ("op_product" in cta) and ("op_offer_number" in cta) and offer_table[cta["offer"]]:
+        product = offer_table[cta["offer"]]
+        if(product == cta["op_product"] and cta["op_offer_number"] == cta["offer"][-3:]):
+            return ""
+        return "op has invalid product or offer number in " + cta["url"]
+
+
--- optool.py
+++ optool.py
@@ -1,22 +1,21 @@
 import unittest
-from urllib import parse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
+import ctalib
 import time
 '''
-Navigation to target page
-Grab all op links in page and print out.
-Assert offer and op match each other based on a op_table dictionary.
-Assert no unrecognized offer and op pairs
-Assert Campaign field is not "XXXX"
-Test passed if all assert pass, otherwise failed.
-
-if new offer and op pairs are found, print out so that they can be copied and added to op_table
-
+https://usa.experian.com/#/registration?offer=at_fcras100&br=exp&op=FRCR-PRD-PCO-100-MQE-RNSCOMP-B0-EXP-GMAC-DIR-XXXXXX-XXXXXX-PHAS2
+cta["offer"] = "at_fcras100"
+cta["op_product"] = "FRCR"
+cta["op_offer_number"] = "100"
+cta["op_campaign"] = "RNSCOMP"
+cta["op_placement"] = "MQE"
+cta["op_recipe"] = "B0"
+cta
 '''
 
 class LandingPageTest(unittest.TestCase):
@@ -30,15 +29,7 @@ class LandingPageTest(unittest.TestCase):
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(10)
         self.mobile = False
-        self.op_table = {
-            'at_fcras100': 'FRCR-PRD-PCO-100',
-            'at_eiwpd102': 'W3DC-PRD-PCO-102',
-            'at_eiwa104': 'WPAC-PRD-PCO-104',
-            'at_eiwb105': 'WPMC-PRD-PCO-105',
-            'at_eiwpa106': 'W3AC-PRD-PCO-106',
-            'at_1b1s109': '1B1S-PRD-PCO-109',
-            'at_3b3s110': '3B3S-PRD-PCO-110'
-        }
+        
 
     def doTest(self):
         # test_url_A = "https://www.experian.com/consumer-products/compare-credit-report-and-score-products-1.html?pc=dir_exp_0"
@@ -60,65 +51,37 @@ class LandingPageTest(unittest.TestCase):
 
     def testMobile(self):
         print("\nTest Mobile")
+        time.sleep(5)
         self.driver.set_window_size(375, 812)
         self.doTest()
+
+    def testPad(self):
+        print("\nTest Pad")
+        time.sleep(5)
+        self.driver.set_window_size(900, 1366)
+        self.doTest()
+    
 
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
 
-    def parseOpLink(self, link):
-        url = link.get_attribute("href")
-        op_data = {}
-        if url and "?" in url:
-            a = url.split('?')
-            op_data = dict(parse.parse_qsl(a[1]))
-            if ("offer" in op_data) and ("op" in op_data):
-                op_data["url"] = url
-                op_data["title"] = link.get_attribute("innerText")
-                op_data["hidden"] = False if link.is_displayed() else True
-                if op_data["op"]:
-                    opComponents = op_data["op"].split("-")
-                    op_data["op_offer"] = "-".join(opComponents[0:4])
-                    op_data["op_postion"] = "-".join(opComponents[4:5])
-                    op_data["op_campaign"] = "-".join(opComponents[5:6])
-                    op_data["op_AB"] = "-".join(opComponents[6:7])
-                    op_data["op_source"] = "-".join(opComponents[7:8])
-                    op_data["op_platform"] = "-".join(opComponents[8:9])
-                    return op_data
-        return {}
-
-    def getOpDataListFromPage(self):
-        time.sleep(5)
-        links = self.driver.find_elements(By.CSS_SELECTOR, "a")
-        op_data_list = []
-        for link in links:
-            op_data = self.parseOpLink(link)
-            if op_data:
-                op_data_list.append(op_data)
-        return op_data_list
-
-    def checkNewOffer(self, op_data_list):
-        new_offer = {}
-        for op_data in op_data_list:
-            if op_data["offer"] not in self.op_table:
-                new_offer[op_data["offer"]] = op_data["op_offer"]
-        if new_offer:
-            print("***find new offers, please add them to op_table in the code.")
-            print(new_offer)
-
     def checkPage(self):
-        op_data_list = self.getOpDataListFromPage()
-        for op_data in op_data_list:
+        links = self.driver.find_elements(By.CSS_SELECTOR, "a")
+        cta_list = ctalib.getCTAListFromLinks(links)
+        for cta in cta_list:
             print("{:20s} {} {}".format(
-                op_data["title"], op_data["url"], "\tH" if op_data["hidden"] else ""))
+                cta["title"], cta["url"], "\tH" if cta["hidden"] else ""))
         print("")
-        self.checkNewOffer(op_data_list)
-        for op_data in op_data_list:
-            self.assertEqual(
-                self.op_table[op_data["offer"]], op_data["op_offer"])
-            self.assertNotEqual(op_data["op_campaign"], "XXXX")
-
-
+        errors = []
+        for cta in cta_list:
+            self.checkError(ctalib.checkMissingOp(cta), errors)
+            self.checkError(ctalib.checkOpOffer(cta), errors)
+            self.assertListEqual(errors, [])
+    
+    def checkError(self, error, errors):
+        if(error):
+            errors.append(error)
+    
 if __name__ == '__main__':
     unittest.main()
