df5bff60648a003c20d050427b597577e08795d8
Jianhua Wang <wjhwsh@Jianhuas-iMac.local>
2018-10-02 18:32:37 -0700

    delete unused files

--- toolcode.py
+++
@@ -1,8 +0,0 @@
-import base64
-print(str)
-#str = b'c2loYW93YW5nNDE5'
-str = input("String to code:")
-encodeStr = base64.b64encode(str.encode('utf-8'))
-print(encodeStr)
-print(base64.b64decode(b'dXRmLTg=').decode('utf-8'))
-
